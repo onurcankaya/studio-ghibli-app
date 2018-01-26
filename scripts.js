7a9072bc5ed052ba8d7d2a504cc1567b5a249e89
Onur Çankaya <onur.eren.cankaya@gmail.com>
2018-01-26 08:48:23 +0100

    created cards with title and description and used textContent to show what returned from the API on the browser

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
+const app = document.getElementById('root')
+
+const logo = document.createElement('img')
+logo.src = 'logo.png'
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(logo)
+app.appendChild(container)
+
 // create a request variable and assign a new XMLHttpRequest object to it
 var request = new XMLHttpRequest()
 
@@ -9,7 +20,26 @@ request.onload = function() {
   var data = JSON.parse(this.response)
 
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(film => console.log(film.title))
+    data.forEach(film => {
+      // create a div with a card class
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
+
+      // create an h1 and set the text content to the film's title
+      const title = document.createElement('h1')
+      title.textContent = film.title
+
+      // create a p and set the text content to the film's description
+      const description = document.createElement('p')
+      description.textContent = `${film.description.substring(0, 300)}...`
+
+      // append the cards to the container element
+      container.appendChild(card)
+      card.appendChild(title)
+      card.appendChild(description)
+
+      console.log(film.title)
+    })
   } else {
     console.log('error')
   }
