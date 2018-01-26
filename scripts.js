182a146d28be7d9dd864c91cfbf1513d2da97cf4
Onur Çankaya <onur.eren.cankaya@gmail.com>
2018-01-26 08:15:03 +0100

    made an http request to the studio ghibli api and fetched the films

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-console.log('studio ghibli app')
+// create a request variable and assign a new XMLHttpRequest object to it
+var request = new XMLHttpRequest()
+
+// open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+
+request.onload = function() {
+  // begin accessing JSON data here
+  var data = JSON.parse(this.response)
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(film => console.log(film.title))
+  } else {
+    console.log('error')
+  }
+}
+
+// send request
+request.send()
